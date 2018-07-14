ce1e9651de1dd283f203444953c053c31276efa3
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 20:21:44 -0700

    Added greyhound class. Filled in methods

---
+++ Lab1/Greyhound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    class Greyhound
+    {
+        public int StartingPosition;
+        public int RaceTrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+
+        public Random Randomizer;
+
+        public bool Run()
+        {
+            Location += Randomizer.Next(1, 5);
+            MyPictureBox.Left = StartingPosition + Location;
+            if (Location >= 25)
+                return true;
+            else
+                return false;
+        }
+    }
+}
