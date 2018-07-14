63ed2647b87d1a36bd2efbee40b5ba900a228d8c
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 20:33:18 -0700

    Finished bet and guy class

---
+++ Lab1/Bet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Bet
+    {
+        public int Amount;
+        public int Dog;
+        public Guy Bettor;
+
+        public string GetDescription()
+        {
+            if (Amount != 0)
+                return Bettor.Name + " bets" + Amount.ToString() + " on dog #" + Dog.ToString();
+            else
+                return Bettor.Name + " hasn't placed a bet";
+        }
+
+        public int PayOut(int Winner)
+        {
+            if (Dog == Winner)
+                return Amount;
+            else
+                return -Amount;
+        }
+    }
+}
---
+++ Lab1/Guy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    class Guy
+    {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+
+        public RadioButton MyRadioButton;
+        public Label MyLabel;
+
+        public void UpdateLabels()
+        {
+            MyLabel.Text = Name + " has " + Cash.ToString() + " bucks";
+        }
+
+        public void ClearBet()
+        {
+            MyBet.Amount = 0;
+        }
+
+        public bool PlaceBet(int BetAmount, int DogToWin)
+        {
+            MyBet = new Bet() { Amount = BetAmount, Dog = DogToWin};
+            if (BetAmount < Cash)
+                return true;
+            else
+                return false;
+        }
+
+        public void Collect(int winner)
+        {
+            Cash += MyBet.PayOut(winner);
+            ClearBet();
+            UpdateLabels();
+
+        }
+    }
+}
